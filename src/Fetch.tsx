f2346f821821ed48e3fb1661fb2460df874a2e44
Guo Yunhe <i@guoyunhe.me>
2023-09-15 23:55:53 +0800

    fetch component

--- src/Fetch.tsx
+++ src/Fetch.tsx
@@ -1,22 +1,61 @@
+import { useEffect, useRef } from 'react';
 import { FetchOptions } from './types';
-import { useFetch } from './useFetch';
+import { UseFetchReturn, useFetch } from './useFetch';
 
-export interface OnInitOptions {
-  reload: () => void;
-  remove: () => void;
+export interface FetchRefAttrs {
+  reload: () => Promise<void>;
+  remove: () => Promise<void>;
 }
 
 export interface FetchProps<T> extends FetchOptions<T> {
   url: string;
-  onInit?: (options: OnInitOptions) => void;
+  onChange?: (result: UseFetchReturn<T>) => void;
 }
 
 /**
  * Component implementation of fast fetch, for class components.
  * Actually, it is only a thin wrapper of the `useFetch()` hook.
+ *
+ * ```jsx
+ * class MyComp extends React.Component {
+ *   state = {
+ *     post: {}, // create a state for post fetch
+ *   }
+ *
+ *   render() {
+ *     const { post } = this.state;
+ *     return <div>
+ *       <Fetch
+ *         url="https://example.com/api/posts/123"
+ *         onChange={result => this.setState({ post: result })}
+ *       />
+ *
+ *       <h1>{post.data?.title}</h1>
+ *       <div>{post.data?.content}</div>
+ *
+ *       <button onClick={post.reload}>Reload</button>
+ *       <button
+ *         onClick={async () => {
+ *           await axios.delete('https://example.com/api/posts/123');
+ *           // clear cache after the resource is deleted from remote
+ *           post.remove();
+ *         }
+ *       >
+ *         Delete
+ *       </button>
+ *     <div>;
+ *   }
+ * }
+ * ```
  */
-export function Fetch<T>({ url, onInit, ...options }: FetchProps<T>) {
-  const { reload, remove } = useFetch(url, options);
-  onInit?.({ reload, remove });
+export function Fetch<T>({ url, onChange, ...options }: FetchProps<T>) {
+  const onChangeRef = useRef(onChange);
+  onChangeRef.current = onChange;
+  const result = useFetch(url, options);
+
+  useEffect(() => {
+    onChangeRef.current?.(result);
+  }, Object.values(result));
+
   return null;
 }
